9010fb745282cee02aa67e8ca550900840e15141
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-07-05 19:14:47 +0530

    Add files via upload

---
+++ 450 DSA Questions/10. Min number of jumps to reach end of array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minJumps(vector<int> vec)
+{
+	vector<int> jumps(vec.size());
+
+	if (vec.size() == 0 || vec[0] == 0)
+		return INT_MAX;
+
+	jumps[0] = 0;
+
+	for (int i = 1; i < vec.size(); i++)
+    {
+		jumps[i] = INT_MAX;
+
+        for (int j = 0; j < i; j++)
+			if (i <= (j + vec[j]) && jumps[j] != INT_MAX)
+            {
+				jumps[i] = min(jumps[i], jumps[j] + 1);
+				break;
+			}
+	}
+
+	return jumps[vec.size() - 1];
+}
+
+int main()
+{
+	vector<int> vec { 1, 2, 3, 4, 5 };
+	cout << "Minimum number of jumps to reach end is " << minJumps(vec);
+}
