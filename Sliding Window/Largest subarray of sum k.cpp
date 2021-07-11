e74c2f712fe7e273cdf8055e7d74d6a1fb081138
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-07-11 19:20:32 +0530

    Update Largest subarray of sum k.cpp

--- Sliding Window/Largest subarray of sum k.cpp
+++ Sliding Window/Largest subarray of sum k.cpp
@@ -16,27 +16,27 @@ int sol(vector<int> vec, int sum)
 {
     int i = 0, j = 0, sub_array_sum = 0, max_len = 0;
 
-    while(j < vec.size())
-        if(sub_array_sum < sum)
+    while(j < vec.size())           // While j doesn't reach end
+        if(sub_array_sum < sum)         // If sum in subarray is smaller than sum, add another value
         {
             sub_array_sum += vec[j];
             j++;
         }
 
-        else if(sub_array_sum == sum)
+        else if(sub_array_sum == sum)       // When subarray sum is equal to required sum, do a max length, increase the size by one
         {
             max_len = max(max_len, j-i);
             sub_array_sum += vec[j];
             j++;
         }
 
-        else if(sub_array_sum > sum)
+        else if(sub_array_sum > sum)        // When subarray sum is greater than required sum, we need to pop elements in subarray
         {
             sub_array_sum -= vec[i];
             i++;
         }
 
-    return max_len;
+    return max_len;     // Returning max length of subarray sum
 }
 
 int main()
